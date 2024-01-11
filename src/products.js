3a942b8791848ce1eb668623b68206f899996a3a
Sanket Kolage <60744054+SanketKolage@users.noreply.github.com>
2024-01-11 13:09:24 +0530

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,61 @@
+import image1 from "./assets/macbook.webp"
+import image2 from "./assets/iphone.png"
+import image3 from "./assets/ipad.png"
+import image4 from "./assets/itv.png"
+import image5 from "./assets/watch.png"
+import image6 from "./assets/speaker.png"
+import image7 from "./assets/powerbank.png"
+import image8 from "./assets/ipod.png"
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: " MacBook Air M1",
+        price: 200000,
+        productImage: image1,
+    },
+    {
+        id: 2,
+        productName: "iPhone 13 Pro Max",
+        price: 150000,
+        productImage: image2,
+    },
+    {
+        id: 3,
+        productName: "iPad Pro ",
+        price: 90000,
+        productImage: image3,
+    },
+    {
+        id: 4,
+        productName: "Apple TV",
+        price: 15000,
+        productImage: image4,
+    },
+    {
+        id: 5,
+        productName: "APPLE Watch Series 6",
+        price: 50000,
+        productImage: image5,
+    },
+    {
+        id: 6,
+        productName: "Speaker",
+        price: 5000,
+        productImage: image6,
+    },
+    {
+        id: 7,
+        productName: "MagSafe Battery Pack",
+        price: 10000,
+        productImage: image7,
+    },
+    {
+        id: 8,
+        productName: "iPod",
+        price: 15000,
+        productImage: image8,
+    },
+
+];
